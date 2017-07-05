d6f1f26a76e4ba59d08e741f28c83283e2c74f14
Brandon Walden <brandonshanewalden@gmail.com>
2017-07-05 11:58:34 -0400

    Random

--- main.js
+++ main.js
@@ -1,10 +1,55 @@
 const computer = 'paper';
-let me = 'rock';
+let me = 'scissors';
 
-if (me === 'paper'); {
-  console.log("Looks like a tie!");
-} else if (me === 'scissors'); {
-  console.log("I win!");
-} else if (me === 'rock'); {
-  console.log("Computer wins!");
+// if (me === 'paper') {
+//   console.log('Looks like a tie!');
+// } else if (me === 'rock') {
+//   console.log('Computer Wins');
+// } else if (me === 'scissors'){
+//   console.log('I Win!');
+// }
+
+
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+    computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+    computerChoice = "paper";
+} else {
+    computerChoice = "scissors";
 }
+
+var compare = function(choice1, choice2) {
+    if(choice1 === choice2) {
+    return "Looks like a tie!";
+}
+if(choice1 === "rock") {
+    if(choice2 === "scissors") {
+        return "rock wins";
+    } else {
+        return "paper wins";
+    }
+}
+if(choice1 === "paper") {
+    if(choice2 === "rock") {
+        return "paper wins";
+    } else {
+        if(choice2 === "scissors") {
+            return "scissors wins";
+    }
+}
+if(choice1 === "scissors") {
+    if(choice2 === "rock") {
+        return "rock wins";
+    } else {
+        if(choice2 === "paper") {
+            return "scissors wins";
+        }
+    }
+}
+}
+};
+console.log("User Choice: " + userChoice);
+console.log("Computer Choice: " + computerChoice);
+compare(userChoice, computerChoice);
