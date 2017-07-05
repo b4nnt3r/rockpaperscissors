06008a0e20b3401cd556f7b137ef6e0045081c72
Brandon Walden <brandonshanewalden@gmail.com>
2017-07-05 11:15:56 -0400

    else if statments

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 const computer = 'paper';
 let me = 'rock';
+
+if (me === 'paper'); {
+  console.log("Looks like a tie!");
+} else if (me === 'scissors'); {
+  console.log("I win!");
+} else if (me === 'rock'); {
+  console.log("Computer wins!");
+}
